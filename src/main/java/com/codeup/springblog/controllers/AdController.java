3a3bfff4ed1454f502d8f1c58a46646a66204399
Carlos Munoz <carlos.munoz.us03@gmail.com>
2021-07-27 10:50:25 -0500

    coplete model binding lecture

--- src/main/java/com/codeup/springblog/controllers/AdController.java
+++ src/main/java/com/codeup/springblog/controllers/AdController.java
@@ -2,19 +2,21 @@ package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.models.Ad;
 import com.codeup.springblog.models.AdRepository;
-import com.codeup.springblog.models.Post;
+
+import com.codeup.springblog.models.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
 
 @Controller
 public class AdController {
     private final AdRepository adDao;
+    private final UserRepository userDao;
 
-    public AdController(AdRepository adDao) {
+    public AdController(AdRepository adDao, UserRepository userDao) {
         this.adDao = adDao;
+        this.userDao = userDao;
     }
 
     @GetMapping("/ads")
@@ -35,5 +37,23 @@ public class AdController {
         Ad ad = adDao.findByTitle(title);
         model.addAttribute("ad", ad);
         return "ads/show";
+    }
+
+    // adding a GetMapping to show the form to create a new Ad.
+    @GetMapping("/ads/create")
+    public String createAdForm(Model model) {
+        // by adding our model as an argument, we can add a newly instantiated Ad object to pass to our form.
+        // This will allow the user to directly correlate the fields with our Ad model.
+        model.addAttribute("ad", new Ad());
+        return "ads/create";
+    }
+
+    @PostMapping("/ads/create")
+    public String createAd(@ModelAttribute Ad ad) {
+        // Once the user submits the form to create an Ad, we want to assign a user to the Ad.
+        ad.setUser(userDao.getById(1L));
+        // Since we have our form binding our model data, we can just just save the Ad directly, without having to use RequestParam.
+        adDao.save(ad);
+        return "redirect:/ads";
     }
 }
